8055f2455dd378e6bfd5d1c7265970a462ebd1bd
ms32v2 <snughacker@gmail.com>
2026-03-12 17:13:23 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,8 @@
 require("dotenv").config();
+
+const mineflayer = require("mineflayer");
+const { pathfinder, Movements, goals } = require("mineflayer-pathfinder");
+const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
 
@@ -10,8 +14,10 @@ const io = new Server(server);
 
 app.use(express.static("panel/public"));
 
-server.listen(3000, () => {
-  console.log("Control panel running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log("Ronny control panel running on port " + PORT);
 });
 
 let manualControl = false;
@@ -19,13 +25,15 @@ let manualControl = false;
 const bot = mineflayer.createBot({
   host: process.env.MC_HOST,
   port: parseInt(process.env.MC_PORT),
-  username: "Ronny"
+  username: "Ronny",
+  version: false
 });
 
 bot.loadPlugin(pathfinder);
 
 bot.once("spawn", () => {
-  console.log("Ronny joined server");
+
+  console.log("Ronny joined the Minecraft server");
 
   const mcData = require("minecraft-data")(bot.version);
   const movements = new Movements(bot, mcData);
@@ -33,9 +41,11 @@ bot.once("spawn", () => {
   bot.pathfinder.setMovements(movements);
 
   randomWalk();
+
 });
 
 function randomWalk() {
+
   setInterval(() => {
 
     if (manualControl) return;
@@ -48,6 +58,7 @@ function randomWalk() {
     bot.pathfinder.setGoal(new goals.GoalBlock(x, pos.y, z));
 
   }, 15000);
+
 }
 
 bot.on("chat", async (username, message) => {
@@ -60,20 +71,27 @@ bot.on("chat", async (username, message) => {
 
   io.emit("chat", `${username}: ${message}`);
   io.emit("chat", `Ronny: ${reply}`);
+
 });
 
 io.on("connection", socket => {
 
   socket.on("say", msg => {
+
     bot.chat(msg);
+
   });
 
   socket.on("roastMode", state => {
+
     setRoastMode(state);
+
   });
 
   socket.on("manual", state => {
+
     manualControl = state;
+
   });
 
 });
